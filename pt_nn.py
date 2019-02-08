5215cfdd5da41704fda2e157375f984a398aedf0
Rob Baldock <rjnbaldock@gmail.com>
2019-02-08 19:55:21 +0100

    Improved docstrings and comments

--- pt_nn.py
+++ pt_nn.py
@@ -5,34 +5,56 @@ import numpy as np
 import time
 
 # Set run parameters
-RUNTOKEN = 1
+RUNTOKEN = 1        # an integer used for labelling the files for this run
 
-NPROC = 16
-NT = 16
-TMIN = 1.0e-2
-TMAX = (1.0e0)
-ABSXMAXFAC = 1.0e3
-GPRIOR_STD = None
-DT_INITIAL = 1.0e-1
+NPROC = 16          # Number of processors to use.
+NT = 16             # Number of temperatures to use.
+TMIN = 1.0e-2       # Lowest temperature in ladder of temperatures.
+TMAX = (1.0e0)      # Maximum temperature in ladder of temperatures. 
+ABSXMAXFAC = 1.0e3  # The parameters of the NN will be constrained to be with a range 
+                    # '[-absxmax,absxmax]' where absxmax = ABSXMAXFAC/sqrt(k_i), with k_i the inward 
+                    # degree of the neuron to which parameter i transmits values.
+GPRIOR_STD = None   # If this is set to a real value then an additional term is applied to (H)MC 
+                    # acceptance/rejection such that the target distribution is proportional to a 
+                    # multivariate Gaussian with this standard deviation for each dimension. 
 
-NUM_TRAJ = 10
-TRAJ_LEN = 100
 
-MAXITER = 100000
-ITERSTOSWAP = 1
-ITERSTOWAYPOINT = 1
-ITERSTOSETDT = 25
-ITERSTOWRITESTATE = 1
+DT_INITIAL = 1.0e-1 # Initial time step (or step size). This will be updated algorithmically, but a 
+                    # good starting point saves time.
 
-N_H_LAYERS = 1
-NODES_PER_H_LAYER = 40
-IMAGE_SIDEL_USE = 16
-DATAPOINTS_PER_CLASS = 50
-DATAFILE = "data50.txt"
-N_CLASSES = 10
+NUM_TRAJ = 10       # The number of trajectories run per iteration.
+TRAJ_LEN = 100      # The number of time steps in a single trajectory.
+
+MAXITER = 100000    # Max number of iterations to run.
+ITERSTOSWAP = 1     # Configuration swaps between neighbouring temperatures are attempted every 
+                    # ITERSTOSWAP iterations.
+ITERSTOWAYPOINT = 1 # Restart information is written after every ITERSTOWAYPOINT iterations.
+ITERSTOSETDT = 25   # The step sizes (or equivalently time steps) are updated after every 
+                    # ITERSTOSETDT interations.
+ITERSTOWRITESTATE = 1 # The latest potential energy values and coordinates are written out after 
+                    # every ITERSTOWRITESTATE iterations.
+
+N_H_LAYERS = 1      # the number of hidden layers.
+NODES_PER_H_LAYER = 40 # the number of nodes in each hidden layer.
+IMAGE_SIDEL_USE = 16 # Images will be transformed to have this many pixels along the side.
+DATAPOINTS_PER_CLASS = 50 # Number of stratified samples to draw per class.
+DATAFILE = "data50.txt" # name of file for storing or recovering the indicies of data points.
+N_CLASSES = 10      # the number of nodes in the final (output) layer.
 
 def calc_numdim(image_sidel_use,n_h_layers,nodes_per_h_layer,n_classes ):
-    """This function calculates the total number of parameters that will feature in the specified NN."""
+    """This function calculates the total number of parameters (weights and biases) that will 
+    feature in the specified NN.
+    
+    Args:
+        image_sidel_use (int) : Images will be transformed to have this many pixels along the side.
+        n_h_layers (int) : the number of hidden layers
+        nodes_per_h_layer (int) : the number of nodes in each hidden layer
+        n_classes (int) : the number of nodes in the final (output) layer
+
+    Return:
+        numdim (int) : the number of parameters that will feature in the NN.
+    
+    """
 
     n_nodes_lowerlayer, n_nodes_thislayer = image_sidel_use**2, nodes_per_h_layer # first hidden layer
     numdim = (1 + n_nodes_lowerlayer) * n_nodes_thislayer
